4f9c2806548841a26eaa430e9e580314884fd24d
Ensify <vignesh1234can@gmail.com>
2024-03-19 10:15:17 +0530

    dashboard

--- monitoring/app.py
+++ monitoring/app.py
@@ -7,8 +7,15 @@ app = Flask(__name__)
 def home():
     m = folium.Map(location=[12.970633350175412, 80.13969680544938], zoom_start=13)
 
-    m.get_root().width = "800px"
-    m.get_root().height = "600px"
+    folium.Marker(
+    location=[12.915571678934642, 80.15392507645515],
+    tooltip="Junction 1",
+    popup= "Cars: 3 | No Issues",
+    icon=folium.Icon(icon="cloud", color="pink"),
+    ).add_to(m)
+
+    # m.get_root().width = "800px"
+    # m.get_root().height = "600px"
     map_iframe = m.get_root()._repr_html_()
 
     return render_template('home.html',map_iframe = map_iframe)
--- object_detection/main.py
+++ object_detection/main.py
@@ -1,11 +1,9 @@
 from ultralytics import YOLO
 
 print("================================")
-model = YOLO(r"models/main2.pt")
+model = YOLO(r"models\best.pt")
 
-results = model.predict(source=r"test_data\Traffic Flow Optiomization and Congestion Management\Problem Statement - 3\Russel_Market_Entrance_PTZ_1.mp4", show=True, stream=True)
+results = model.track(source=r"test_data\Traffic Flow Optiomization and Congestion Management\Problem Statement - 3\Nr_ABVMCRI_Gate_FIX_1.mp4", show=True)
 
-for i in range(1000):
-    next(results)
 
 #['orig_img', 'orig_shape', 'boxes', 'masks', 'probs', 'keypoints', 'obb', 'speed', 'names', 'path', 'save_dir', '_keys']
