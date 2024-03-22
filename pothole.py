87a3a53882ee4759e182749ab7f5d107b7d44070
Ensify <vignesh1234can@gmail.com>
2024-03-22 10:21:15 +0530

    pothole

---
+++ pothole.py
@@ -0,0 +1,18 @@
+# Import the InferencePipeline object
+from inference import InferencePipeline
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# Import the built in render_boxes sink for visualizing results
+from inference.core.interfaces.stream.sinks import render_boxes
+# initialize a pipeline objectq
+pipeline = InferencePipeline.init(
+    model_id="pothole-detection-i00zy/2", # Roboflow model to use
+    video_reference="mixkit-potholes-in-a-rural-road-25208-large.mp4", # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
+    on_prediction=render_boxes, # Function to run after each prediction
+    api_key= os.environ.get('API_KEY')
+)
+
+pipeline.start()
+pipeline.join()
