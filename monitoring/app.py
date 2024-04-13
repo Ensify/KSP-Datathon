1f3b38de1ea65882703bd85f54a243f4ce8ba42d
SarumathyPrabakaran <harinisarumathi@gmail.com>
2024-04-14 00:43:23 +0530

    alerts feature

--- monitoring/app.py
+++ monitoring/app.py
@@ -1,17 +1,35 @@
 from flask import Flask,render_template, jsonify
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from bson import ObjectId
+import os, json
+# from database.models import Alert
+from datetime import datetime, timedelta
+
+
 
 load_dotenv()
-import os, json
 client = MongoClient(os.environ.get('MONGO_URI'))
+scheduler = BackgroundScheduler()
+
 db = client['ksp-traffic']
 node_collection = db['nodes']
 event_collection = db['events']
 instance_collection = db['instances']
+alert_collection = db['alerts']
 
 app = Flask(__name__)
+
+
+class Alert:
+    def __init__(self, event_id, node_id, start_time):
+        self.event_id = event_id
+        self.node_id = node_id
+        self.start_time = start_time
+        self.alert_time = datetime.now()
+
 
 @app.route("/")
 def home():
@@ -29,10 +47,20 @@ def home():
     return render_template('home.html', result=results)
 
 
-@app.route('/alert')
-def alert():
-    pass
-    #prometheus code to create an alert.
+@app.route('/alerts')
+def get_alerts():
+    print("Alerts Triggered")
+    check_events_for_alerts()
+    alerts = list(alert_collection.find({}))
+    alerts = [{
+        'eventId': alert['event_id'],
+        'nodeId': alert['node_id'],
+        'startTime': alert['start_time'],
+        'alertTime': alert['alert_time']
+    } for alert in alerts]
+    alerts = alerts[::-1]
+    return jsonify(alerts)
+
 
 @app.route('/data')
 def get_data():
@@ -57,6 +85,38 @@ def get_events(node_id):
 
     print(result)
     return jsonify(result)
+
+
+def create_or_update_alert(event_id, node_id, start_time):
+    existing_alert = alert_collection.find_one({"event_id": event_id})
+    
+    if existing_alert:
+        alert_collection.update_one({"event_id": event_id}, {"$set": {"alert_time": datetime.now()}})
+    else:
+        alert = Alert(event_id, node_id, start_time)
+        alert_collection.insert_one(alert.__dict__)
+
+
+def check_events_for_alerts():
+  
+    events = event_collection.find({"end_time": None})
+
+    for event in events:
+        start_time = event['start_time']
+        current_time = datetime.now()
+        
+ 
+        duration = current_time - start_time
+        
+ 
+        if duration > timedelta(minutes=15):
+            print("Alert: Event duration exceeded 15 minutes")
+            create_or_update_alert(event["id"], event["node_id"], event["start_time"])
+            event_collection.update_one({"id": event["id"]}, {"$inc": {"alerts_raised": 1}})
+
+
+scheduler.add_job(check_events_for_alerts, 'interval', minutes=6)
+scheduler.start()
 
 
 if __name__ == "__main__":
